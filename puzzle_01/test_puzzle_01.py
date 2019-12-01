2e5fbfd78a39c7d7794c1a908b66e060cacf690d
Paul Reiter <reiter.paul@gmail.com>
2019-12-01 23:03:36 +0100

    Apply `black` to all python files for solution #1

--- puzzle_01/test_puzzle_01.py
+++ puzzle_01/test_puzzle_01.py
@@ -2,20 +2,11 @@ import pytest
 from puzzle_01 import calculate_fuel_requirement, calculate_recursive_fuel_requirement
 
 
-@pytest.mark.parametrize('mass, fuel', [
-    (12, 2),
-    (14, 2),
-    (1969, 654),
-    (100756, 33583),
-])
+@pytest.mark.parametrize("mass, fuel", [(12, 2), (14, 2), (1969, 654), (100756, 33583)])
 def test_calculate_fuel_requirement(mass, fuel):
     assert calculate_fuel_requirement(mass) == fuel
 
 
-@pytest.mark.parametrize('mass, fuel', [
-    (14, 2),
-    (1969, 966),
-    (100756, 50346),
-])
+@pytest.mark.parametrize("mass, fuel", [(14, 2), (1969, 966), (100756, 50346)])
 def test_calculate_recursive_fuel_requirement(mass, fuel):
     assert calculate_recursive_fuel_requirement(mass) == fuel
