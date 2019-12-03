8f4ea4b2b557a7e590d05eada60ec9d163b2e121
Paul Reiter <reiter.paul@gmail.com>
2019-12-03 22:51:17 +0100

    Solve part 1 of puzzle #3

---
+++ puzzle_03/puzzle_03.py
@@ -0,0 +1,56 @@
+from collections import namedtuple
+
+Point = namedtuple("Point", "x, y")
+
+
+def add(self, other):
+    return Point(self.x + other.x, self.y + other.y)
+
+
+Point.__add__ = add
+
+
+DIRECTION_DICT = {
+    "U": Point(0, 1),
+    "D": Point(0, -1),
+    "L": Point(-1, 0),
+    "R": Point(1, 0),
+}
+
+
+def points_from_path(path):
+    path_points = set()
+    position = Point(0, 0)
+    for move in path:
+        direction, distance = DIRECTION_DICT[move[0]], int(move[1:])
+        for _ in range(distance):
+            position = position + direction
+            path_points.add(position)
+    return path_points
+
+
+def min_crossing_distance_from_points(path_points1, path_points2):
+    crossing_points = path_points1.intersection(path_points2)
+    manhatten_distances = set(abs(point.x) + abs(point.y) for point in crossing_points)
+    return min(manhatten_distances)
+
+
+def min_crossing_point_distance(path1, path2):
+    return min_crossing_distance_from_points(
+        points_from_path(path1.split(",")), points_from_path(path2.split(","))
+    )
+
+
+def read_input():
+    with open("input.txt", "r") as stream:
+        path1 = stream.readline()
+        path2 = stream.readline()
+    return path1, path2
+
+
+def solve_part_1():
+    return min_crossing_point_distance(*read_input())
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {solve_part_1()}")
---
+++ puzzle_03/test_puzzle_03.py
@@ -0,0 +1,23 @@
+import pytest
+from puzzle_03 import Point, points_from_path, min_crossing_point_distance
+
+
+def test_points_from_path():
+    assert points_from_path(["U2", "R1", "D1"]) == set(
+        (Point(0, 1), Point(0, 2), Point(1, 2), Point(1, 1))
+    )
+
+
+@pytest.mark.parametrize(
+    "path1, path2, distance",
+    [
+        ("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 159),
+        (
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+            135,
+        ),
+    ],
+)
+def test_cde(path1, path2, distance):
+    assert min_crossing_point_distance(path1, path2) == distance
