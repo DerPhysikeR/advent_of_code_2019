aa7cd18f52d5c2b946696d586127111381b1b237
Paul Reiter <reiter.paul@gmail.com>
2019-12-03 23:26:46 +0100

    Solve part 2 of puzzle #3

--- puzzle_03/puzzle_03.py
+++ puzzle_03/puzzle_03.py
@@ -19,8 +19,8 @@ DIRECTION_DICT = {
 
 
 def points_from_path(path):
-    path_points = []
     position = Point(0, 0)
+    path_points = [position]
     for move in path:
         direction, distance = DIRECTION_DICT[move[0]], int(move[1:])
         for _ in range(distance):
@@ -31,12 +31,27 @@ def points_from_path(path):
 
 def min_crossing_distance_from_points(path_points1, path_points2):
     crossing_points = set(path_points1).intersection(set(path_points2))
+    crossing_points.remove(Point(0, 0))
     manhatten_distances = set(abs(point.x) + abs(point.y) for point in crossing_points)
     return min(manhatten_distances)
 
 
 def min_crossing_point_distance(path1, path2):
     return min_crossing_distance_from_points(
+        points_from_path(path1.split(",")), points_from_path(path2.split(","))
+    )
+
+
+def min_crossing_distance_along_paths_from_points(path_points1, path_points2):
+    crossing_points = set(path_points1).intersection(set(path_points2))
+    crossing_points.remove(Point(0, 0))
+    return min(
+        path_points1.index(cp) + path_points2.index(cp) for cp in crossing_points
+    )
+
+
+def min_crossing_distance_along_paths(path1, path2):
+    return min_crossing_distance_along_paths_from_points(
         points_from_path(path1.split(",")), points_from_path(path2.split(","))
     )
 
@@ -52,5 +67,10 @@ def solve_part_1():
     return min_crossing_point_distance(*read_input())
 
 
+def solve_part_2():
+    return min_crossing_distance_along_paths(*read_input())
+
+
 if __name__ == "__main__":
     print(f"Part 1: {solve_part_1()}")
+    print(f"Part 2: {solve_part_2()}")
--- puzzle_03/test_puzzle_03.py
+++ puzzle_03/test_puzzle_03.py
@@ -1,9 +1,15 @@
 import pytest
-from puzzle_03 import Point, points_from_path, min_crossing_point_distance
+from puzzle_03 import (
+    Point,
+    points_from_path,
+    min_crossing_point_distance,
+    min_crossing_distance_along_paths,
+)
 
 
 def test_points_from_path():
     assert tuple(points_from_path(["U2", "R1", "D1"])) == (
+        Point(0, 0),
         Point(0, 1),
         Point(0, 2),
         Point(1, 2),
@@ -22,5 +28,20 @@ def test_points_from_path():
         ),
     ],
 )
-def test_cde(path1, path2, distance):
+def test_min_crossing_point_distance(path1, path2, distance):
     assert min_crossing_point_distance(path1, path2) == distance
+
+
+@pytest.mark.parametrize(
+    "path1, path2, distance",
+    [
+        ("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83", 610),
+        (
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+            410,
+        ),
+    ],
+)
+def test_min_crossing_distance_along_paths(path1, path2, distance):
+    assert min_crossing_distance_along_paths(path1, path2) == distance
