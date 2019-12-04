4089fb212bc1a3c4ef8ccb126d95f75ea8a426ff
Paul Reiter <reiter.paul@gmail.com>
2019-12-04 21:36:06 +0100

    Solve part 2 of puzzle #4

--- puzzle_04/puzzle_04.py
+++ puzzle_04/puzzle_04.py
@@ -6,6 +6,18 @@ def has_equal_neighboring_digits(digits):
     return any(digits[i] == digits[i + 1] for i in range(len(digits) - 1))
 
 
+def digit_block_lengths(digits):
+    block_lengths = [1]
+    previous_digit = digits[0]
+    for digit in digits[1:]:
+        if previous_digit == digit:
+            block_lengths[-1] += 1
+            continue
+        block_lengths.append(1)
+        previous_digit = digit
+    return block_lengths
+
+
 def monotonic_increasing(digits):
     return all(digits[i] <= digits[i + 1] for i in range(len(digits) - 1))
 
@@ -21,9 +33,25 @@ def is_password_valid(password):
     return True
 
 
+def is_password_strictly_valid(password):
+    if not is_password_valid(password):
+        return False
+    digits = to_digits(password)
+    if 2 not in digit_block_lengths(digits):
+        return False
+    return True
+
+
 def solve_part_1():
     return sum(is_password_valid(password) for password in range(138241, 674034 + 1))
 
 
+def solve_part_2():
+    return sum(
+        is_password_strictly_valid(password) for password in range(138241, 674034 + 1)
+    )
+
+
 if __name__ == "__main__":
     print(f"Part 1: {solve_part_1()}")
+    print(f"Part 2: {solve_part_2()}")
--- puzzle_04/test_puzzle_04.py
+++ puzzle_04/test_puzzle_04.py
@@ -1,5 +1,5 @@
 import pytest
-from puzzle_04 import is_password_valid
+from puzzle_04 import is_password_valid, is_password_strictly_valid
 
 
 def test_int_to_digits():
@@ -17,5 +17,23 @@ def test_int_to_digits():
         (145678, False),
     ],
 )
-def test_is_valid(password, is_valid):
+def test_is_password_valid(password, is_valid):
     assert is_password_valid(password) == is_valid
+
+
+@pytest.mark.parametrize(
+    "password, is_valid",
+    [
+        (222222, False),
+        (2222222, False),
+        (111111, False),
+        (223450, False),
+        (123789, False),
+        (145678, False),
+        (123444, False),
+        (111122, True),
+        (112233, True),
+    ],
+)
+def test_is_password_strictly_valid(password, is_valid):
+    assert is_password_strictly_valid(password) == is_valid
