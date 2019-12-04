1939c0dc057d81268abd8b54cb64a40351a59e16
Paul Reiter <reiter.paul@gmail.com>
2019-12-04 21:51:00 +0100

    DRY for puzzle input

--- puzzle_04/puzzle_04.py
+++ puzzle_04/puzzle_04.py
@@ -42,16 +42,17 @@ def is_password_strictly_valid(password):
     return True
 
 
-def solve_part_1():
-    return sum(is_password_valid(password) for password in range(138241, 674034 + 1))
+def solve_part_1(lower, upper):
+    return sum(is_password_valid(password) for password in range(lower, upper + 1))
 
 
-def solve_part_2():
+def solve_part_2(lower, upper):
     return sum(
-        is_password_strictly_valid(password) for password in range(138241, 674034 + 1)
+        is_password_strictly_valid(password) for password in range(lower, upper + 1)
     )
 
 
 if __name__ == "__main__":
-    print(f"Part 1: {solve_part_1()}")
-    print(f"Part 2: {solve_part_2()}")
+    lower, upper = 138241, 674034
+    print(f"Part 1: {solve_part_1(lower, upper)}")
+    print(f"Part 2: {solve_part_2(lower, upper)}")
